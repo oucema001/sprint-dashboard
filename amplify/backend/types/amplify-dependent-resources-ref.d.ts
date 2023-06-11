6f9822dc07720d573c1ef25a3ac8aaf0cb0016bc
Oussama Rajhi <oussama@Oussamas-Mac-mini.local>
2023-06-11 17:04:18 +0200

    change lambda

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,36 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "AdminQueries": {
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "sprintdashboard34543847": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "AdminsGroupRole": "string",
+      "UsersGroupRole": "string"
+    }
+  },
+  "function": {
+    "AdminQueriesa0c6c8a8": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "sprintdashboard34543847PostConfirmation": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
